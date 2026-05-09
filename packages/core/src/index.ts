aae8716a62dbd5ab59070c29c9bf21b322dceca4
Eugen Reinfeld <eugen@reinfeld.me>
2026-05-09 14:59:29 +0200

    chore(core): re-export public API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,7 @@
+export * from "./domain/index.js";
+export * from "./store/index.js";
+export * from "./sync/index.js";
+export * from "./auth/index.js";
+export * from "./supabase/index.js";
+
 export const PULSE_CORE_VERSION = "0.1.0";
